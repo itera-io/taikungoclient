f5b6c5727dae3d3daef26d250cfa00fe73899a4f
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-10-04 14:40:02 +0200

    fix(client generation): correct path of client.go file

--- .github/generate_client.sh
+++ .github/generate_client.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Module name
-module_name="github.com/itera-io/taikungoclient"
+module_parent_path="github.com/itera-io"
+module_name="taikungoclient"
 
 # Delete client/ and models/ directories if they already exist
 [ -d client ] && rm -rf client && echo "Removed client/ directory"
@@ -14,22 +15,22 @@ module_name="github.com/itera-io/taikungoclient"
 [ -f go.sum ] && rm -rf go.sum && echo "Removed go.sum file"
 
 # Ensure go-swagger names the package for the 'Documentation' endpoint 'doc' instead of 'documentation'
-sed 's/"Documentation"/"Doc"/g' swagger.json >swagger-patch.json
+sed 's/"Documentation"/"Doc"/g' swagger.json > swagger-patch.json
 
 # Initialize go module
 go mod init "${module_name}" && echo "Initialized ${module_name} module"
 
 # Generate the client
 echo -n "Generating client..."
-swagger generate client -c "client" -f swagger-patch.json -A "${module_name}" $@
+swagger generate client -c "client" -f swagger-patch.json -A "${module_parent_path}/${module_name}" $@
 rm -f swagger-patch.json
 echo -e " Done."
 
 # Insert code to support extra media types
 reference_line="transport := httptransport.New"
-client_definition_file="client/${module_name}"_client.go
+client_definition_file="client/${module_name}_client.go"
 sed -i "/${reference_line}/r media_type_support_code.go" "${client_definition_file}"
 go fmt "${client_definition_file}"
 
 # Tidy go module
-go mod tidy && echo "Tidied ${module_name} module"
+go mod tidy && echo "Tidied ${module_parent_path}/${module_name} module"
