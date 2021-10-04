a34a9354a8a811c7a8d1fe990a7971827e86f8e5
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-04 15:01:26 +0200

    fix(client generation): truly correct path of client definition file

--- .github/generate_client.sh
+++ .github/generate_client.sh
@@ -3,34 +3,31 @@
 set -e
 
 # Module name
-module_parent_path="github.com/itera-io"
-module_name="taikungoclient"
+module_name="github.com/itera-io/taikungoclient"
+client_definition_file="client/github_com_itera_io_taikungoclient_client.go"
 
 # Delete client/ and models/ directories if they already exist
-[ -d client ] && rm -rf client && echo "Removed client/ directory"
-[ -d models ] && rm -rf models && echo "Removed models/ directory"
+rm -rfv client/
+rm -rfv models/
 
 # Delete go.mod and go.sum files if they already exist
-[ -f go.mod ] && rm -rf go.mod && echo "Removed go.mod file"
-[ -f go.sum ] && rm -rf go.sum && echo "Removed go.sum file"
+rm -rfv go.mod
+rm -rfv go.sum
 
 # Ensure go-swagger names the package for the 'Documentation' endpoint 'doc' instead of 'documentation'
 sed 's/"Documentation"/"Doc"/g' swagger.json > swagger-patch.json
 
 # Initialize go module
-go mod init "${module_name}" && echo "Initialized ${module_name} module"
+go mod init "${module_name}"
 
 # Generate the client
-echo -n "Generating client..."
-swagger generate client -c "client" -f swagger-patch.json -A "${module_parent_path}/${module_name}" $@
+swagger generate client -c "client" -f swagger-patch.json -A "${module_name}" $@
 rm -f swagger-patch.json
-echo -e " Done."
 
 # Insert code to support extra media types
 reference_line="transport := httptransport.New"
-client_definition_file="client/${module_name}_client.go"
-sed -i "/${reference_line}/r media_type_support_code.go" "${client_definition_file}"
+sed -i "/${reference_line}/r .github/codegen/media_type_support_code.go" "${client_definition_file}"
 go fmt "${client_definition_file}"
 
 # Tidy go module
-go mod tidy && echo "Tidied ${module_parent_path}/${module_name} module"
+go mod tidy
