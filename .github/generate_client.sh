bd584d5b520410bbf133c3f3f3c27abd06475a0d
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-21 16:20:38 +0200

    feat: make datetimes and showbackruleid nullable

--- .github/generate_client.sh
+++ .github/generate_client.sh
@@ -19,17 +19,24 @@ rm -rfv go.sum
 # Ensure go-swagger names the package for the 'Documentation' endpoint 'doc' instead of 'documentation'
 sed 's/"Documentation"/"Doc"/g' swagger.json > swagger-patch.json
 
+# Make all datetimes nullable
+sed -i 's/"format": "date-time"/"x-nullable": true, "format": "date-time"/g' swagger-patch.json
+
+# Make showback_credential_id nullable
+refline="\"showbackCredentialId\":"
+sed -i "/${refline}/a \"x-nullable\": true," swagger-patch.json
+
 # Initialize go module
 go mod init "${module_name}"
 
 # Generate the client
-./swagger generate client -f swagger-patch.json -A "${app_name}" $@
+swagger generate client -f swagger-patch.json -A "${app_name}" $@
 rm -f swagger-patch.json
 
 # Insert code to support extra media types
-reference_line="transport := httptransport.New"
+refline="transport := httptransport.New"
 client_definition_file="client/${app_name}_client.go"
-sed -i "/${reference_line}/r .github/codegen/media_type_support_code.go" "${client_definition_file}"
+sed -i "/${refline}/r .github/codegen/media_type_support_code.go" "${client_definition_file}"
 go fmt "${client_definition_file}"
 
 # Tidy go module
