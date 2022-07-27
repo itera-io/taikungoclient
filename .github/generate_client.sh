93862a8e28d62271889ef2e04274b2ccdcf50e7b
mirsahib <mirsahib.movsumov@itera.io>
2022-07-27 16:05:04 +0400

    feat: add showback endpoints to the client

--- .github/generate_client.sh
+++ .github/generate_client.sh
@@ -18,28 +18,35 @@ rm -rfv go.sum
 
 # Ensure go-swagger names the package for the 'Documentation' endpoint 'doc' instead of 'documentation'
 sed 's/"Documentation"/"Doc"/g' swagger.json >swagger-patch.json
+sed 's/"Documentation"/"Doc"/g' showback-swagger.json >showback-swagger-patch.json
 
 # Make all datetimes nullable
 sed -i 's/"format": "date-time"/"x-nullable": true, "format": "date-time"/g' swagger-patch.json
+sed -i 's/"format": "date-time"/"x-nullable": true, "format": "date-time"/g' showback-swagger-patch.json
 
 # Make showback_credential_id nullable
 refline="\"showbackCredentialId\":"
 sed -i "/${refline}/a \"x-nullable\": true," swagger-patch.json
+sed -i "/${refline}/a \"x-nullable\": true," showback-swagger-patch.json
 
 # Remove omitempty
 refline="\"(ruleDiscountRate|globalDiscountRate|discountRate|displayName)\":"
 sed -Ei "/${refline}/a \"x-omitempty\": false," swagger-patch.json
+sed -Ei "/${refline}/a \"x-omitempty\": false," showback-swagger-patch.json
 
 # Remove omitempty
 refline="\"type\": \"boolean\""
 sed -i "s/${refline}/\"x-omitempty\": false,${refline}/g" swagger-patch.json
+sed -i "s/${refline}/\"x-omitempty\": false,${refline}/g" showback-swagger-patch.json
 
 # Initialize go module
 go mod init "${module_name}"
 
 # Generate the client
 ./swagger generate client -f swagger-patch.json -A "${app_name}" $@
+./swagger generate client -f showback-swagger-patch.json -A "${app_name}" $@
 rm -f swagger-patch.json
+rm -f showback-swagger-patch.json
 
 # Insert code to support extra media types
 refline="transport := httptransport.New"
