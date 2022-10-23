d8b4ee3c1ed48d30c489c7f365b8de8620bdf598
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-10-23 19:17:34 +0200

    feat: generate client using go-swagger with-expand
    Try generating client with the --with-expand option in the hope that
    this will fix the current issue with error output.
    
    From https://goswagger.io/usage/expand.html:
    > Expanding a specification resolve all $ref (remote or local) and
    > replace them by their expanded content in the main spec document.
    
    The type-400 responses that return an array of Error models (formally
    ValidationProblemDetails) are currently displayed as an array of
    addresses in memory:
    
    For example in the Taikun CLI:
    ```
    > taikun access-profile add ""
    Error: [POST /api/v{v}/AccessProfiles/create][400] accessProfilesCreateBadRequest  [0xc0002dc040 0xc0002dc0a0]
    ```
    
    The desired output is as follows:
    ```
    Error: [POST /api/v{v}/AccessProfiles/create][400] accessProfilesCreateBadRequest [
      {
        "code": "access_profile_name",
        "description": "access_profile_name can not be null or empty"
      },
      {
        "code": "access_profile_name",
        "description": "access_profile_name should contain at least 3 characters"
      },
      {
        "code": "access_profile_name",
        "description": "Please specify only alpha numeric characters or non alpha numeric (-)"
      }
    ]
    ```

--- .github/generate_client.sh
+++ .github/generate_client.sh
@@ -10,6 +10,9 @@ app_name="taikungoclient"
 showback_app_name="showbackgoclient"
 showback_package_name="showbackclient"
 
+# Swagger generate client options
+readonly swagger_generate_client_options="--with-expand"
+
 # Temporary swagger patch files
 readonly swagger_patch_file="swagger-patch.json"
 readonly showback_swagger_patch_file="showback-swagger-patch.json"
@@ -54,13 +57,13 @@ go mod init "${module_name}"
 # Generate the client
 swagger generate client \
   -f "${swagger_patch_file}" \
-  -A "${app_name}" $@
+  -A "${app_name}" ${swagger_generate_client_options} $@
 
 # Generate showback client
 swagger generate client \
   -f "${showback_swagger_patch_file}" \
   -c "${showback_package_name}" \
-  -A "${showback_app_name}" $@
+  -A "${showback_app_name}" ${swagger_generate_client_options} $@
 
 # Remove swagger patch files
 rm -f "${swagger_patch_file}"
