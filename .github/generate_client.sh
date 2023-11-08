da72d7c84643ef6e62e7b78f26cb687b1f1e6e0e
Radek Smid <radek.smid@taikun.cloud>
2023-11-08 10:19:04 +0100

    test: CI automatic client regeneration.

---
+++ .github/generate_client.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Module name
+GITHUB_USERNAME="itera-io"
+GITHUB_REPO="taikungoclient"
+
+FILE_WEB="swagger-web.json"
+FILE_SHOWBACK="swagger-showback.json"
+
+#MAIN_API_URL="https://api.taikun.dev/swagger/v1/swagger.json"
+#SHOWBACK_API_URL="https://api.taikun.dev/showback/swagger/v1/swagger.json"
+#rm "$FILE_WEB" "$FILE_SHOWBACK"
+#
+#wget "$MAIN_API_URL" -O "./$FILE_WEB"
+#wget "$SHOWBACK_API_URL" -O "./$FILE_SHOWBACK"
+
+rm -rf ./client         # Generator refuses to rewrite some files
+rm -rf ./showbackclient # Generator refuses to rewrite some files
+
+# Generate client
+java -jar openapi-generator-cli.jar generate -i ./"$FILE_WEB" \
+-g go \
+--additional-properties=packageName=taikuncore \
+--additional-properties=enumClassPrefix=true \
+--git-user-id="$GITHUB_USERNAME" \
+--git-repo-id="$GITHUB_REPO/client" \
+-o ./client
+
+openapi-generator-cli generate -i ./"$FILE_SHOWBACK" \
+-g go \
+--additional-properties=packageName=taikunshowback  \
+--additional-properties=enumClassPrefix=true \
+--git-user-id="$GITHUB_USERNAME" \
+--git-repo-id="$GITHUB_REPO/showbackclient" \
+-o ./showbackclient
+
---
+++ .github/should_regenerate_client.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Sha256sum file paths
+old_sha1sum_path="./.github/swagger_sha1sum.txt"
+new_sha1sum_path="./.github/swagger_sha1sum_new.txt"
+
+# Combine swagger files
+cat swagger-web.json swagger-showback.json > combined-swagger.json
+# Generate new sha1sum of swagger.json
+sha1sum combined-swagger.json > "${new_sha1sum_path}"
+# Remove combined-swagger.json file
+rm -f combined-swagger.json
+# If running for the first time, save the sha1sum and return true
+if [[ ! -f "${old_sha1sum_path}" ]]; then
+  mv "${new_sha1sum_path}" "${old_sha1sum_path}"
+  echo "Running for the first time, saved ${old_sha1sum_path} and exiting with code 0"
+  exit 0
+fi
+
+# If sha1sums differ, return true and save new sha1sum, otherwise return false
+if diff "${new_sha1sum_path}" "${old_sha1sum_path}" &>/dev/null; then
+  rm -f "${new_sha1sum_path}"
+  echo "swagger.json hasn't changed, exiting with code 1"
+  exit 1
+else
+  mv "${new_sha1sum_path}" "${old_sha1sum_path}"
+  echo "swagger.json updated, saved ${old_sha1sum_path} and exiting with code 0"
+  exit 0
+fi
