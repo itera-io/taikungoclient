bc799aab7e9bc21205cd6a60184bccf78bf7729b
Ivan Eroshkin <ieroshkin@cloudera.com>
2026-07-29 12:04:10 +0200

    Fixing go.mod

--- taikungoclient.go
+++ taikungoclient.go
@@ -88,9 +88,8 @@ func (c *customTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 		if c.Client.token == "" {
 			// No token yet: login with email + password
 			loginCmd := taikuncore.LoginCommand{
-				AccountName: *taikuncore.NewNullableString(&c.Client.accountName),
-				Email:       *taikuncore.NewNullableString(&c.Client.email),
-				Password:    *taikuncore.NewNullableString(&c.Client.password),
+				Email:    *taikuncore.NewNullableString(&c.Client.email),
+				Password: *taikuncore.NewNullableString(&c.Client.password),
 			}
 			if c.Client.authMode != "" {
 				loginCmd.Mode = *taikuncore.NewNullableString(&c.Client.authMode)
